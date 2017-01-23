8c7bb4384845cf6f15b41986e84d23d2f0b0a469
caoyongzheng <15267056106@139.com>
2017-01-23 17:10:03 +0800

    carousel2.html

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express')
-
 const app = new express()
+const os = require('os')
 
 app.use(express.static(__dirname))
 
 app.listen(8000, function(err) {
+  const network = os.networkInterfaces()
+  const en0 = network.en0
   if (err) {
     console.error(err)
   } else {
-    console.log('Page is running at: http://localhost:8000/src')
+    console.log(`Page is running at: http://${en0[1].address}:8000/src`)
   }
 })
