4d0761a5e11a766bdcdd306d9a7cbc39eaf9c17f
caoyongzheng <15267056106@139.com>
2017-02-15 21:16:12 +0800

    image

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express')
-const app = new express()
+const path = require('path')
 const os = require('os')
+const chokidar = require('chokidar')
+
+const app = new express()
 
 app.use(express.static(__dirname))
 
@@ -13,3 +16,12 @@ app.listen(8000, function(err) {
     console.log(`Page is running at: http://${en0[1].address}:8000/src`)
   }
 })
+
+// const watcher = chokidar.watch('./src', {
+//   ignored: /(^|[\/\\])\../,
+//   persistent: true
+// })
+//
+// watcher.on('all', function(event, path) {
+//   console.log(event, path);
+// })
