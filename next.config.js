dbd73cbb0feaf7aec81b063863b2f9084340ccdc
caoyongzheng <15267056106@139.com>
2018-04-21 18:56:56 +0800

    add .nojekyll

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 const glob = require('glob')
-const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  assetPrefix: isProd ? 'https://raw.githubusercontent.com/caoyongzheng/web-demos/gh-pages' : '',
   exportPathMap() {
     const pathMap = {}
     glob.sync('pages/**/*.js', { ignore: 'pages/_document.js' }).forEach(s => {
