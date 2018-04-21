d26bf0bacd202b14c9c063eb8c2a735395122072
caoyongzheng <15267056106@139.com>
2018-04-21 18:02:44 +0800

    cdn

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 const glob = require('glob')
+const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
+  assetPrefix: isProd ? 'https://raw.githubusercontent.com/caoyongzheng/web-demos/gh-pages' : '',
   exportPathMap() {
     const pathMap = {}
     glob.sync('pages/**/*.js', { ignore: 'pages/_document.js' }).forEach(s => {
